5f235859e71215a80509aea113550a8c4df81438
Wilker <Wilker.rj@gmail.com>
2018-10-02 13:07:50 -0300

    Mapeamento direto funcionando

--- mapping/direct.rb
+++ mapping/direct.rb
@@ -1,6 +1,19 @@
-class Mapping::Direct
-  # Este método recebe o dado que precisa ser salvo e o tamanho da cache para em qual quadro deverá ser salvo o dado
-  def self.select(block, lenght)
-    block % lenght
+module Mapping
+  class Direct
+    attr_reader :hits, :misses, :mem, :lenght
+
+    def initialize(lenght)
+      @hits = 0
+      @misses = 0
+      @lenght = lenght
+      @mem = []
+      (0..lenght).each{|x| @mem[x] = -1 }
+    end
+
+    def read(block)
+      return @hits +=1 if @mem.include?(block)
+      @misses +=1
+      @mem[block % @lenght] = block
+    end
   end
 end
