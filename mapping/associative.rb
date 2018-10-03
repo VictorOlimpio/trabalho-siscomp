027fe7a1856ff2becdf3a3e9775e64ddcb83ee5b
Wilker <Wilker.rj@gmail.com>
2018-10-03 02:22:01 -0300

    Corrigindo associativa para poder usar Fifo.

--- mapping/associative.rb
+++ mapping/associative.rb
@@ -13,11 +13,21 @@ module Mapping
     end
 
     def read(block)
-      return @hits += 1 if @mem.include?(block)
+      if @mem.include?(block)
+        pos = @mem.find_index(block)
+        return cache_hit(aux,@subst,0,pos)
+      end
+      # return @hits += 1 if @mem.include?(block)
       @misses += 1
       subst = @subst.select(@aux)
-      @subst.update(aux, subst, 0)
+      @subst.update(aux, subst, 0,0)
       @mem[subst] = block
+    end
+
+    # Existem políticas de substiuição que devem ser atualizadas em caso de cache hit
+    def cache_hit(aux, subst, init, pos)
+      @hits = hits+1
+      subst.cache_hit(aux, init, pos)
     end
   end
 end
